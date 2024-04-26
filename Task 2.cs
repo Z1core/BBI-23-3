6795b1d6480a4688d929ce089bd7fbd4aeb0ff94
Reshetkov <64368633+Z1core@users.noreply.github.com>
2024-04-26 13:59:49 +0300

    Add files via upload

---
+++ Task 2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputText = Console.ReadLine();
+        string outputText = ReverseSentences(inputText);
+        Console.WriteLine(outputText);
+    }
+
+    static string ReverseSentences(string text)
+    {
+        string pattern = @"(?<=\.|\?|!)\s+";
+
+        string[] sentences = Regex.Split(text, pattern);
+
+        for (int i = 0; i < sentences.Length; i++)
+        {
+            sentences[i] = ReverseSentence(sentences[i]);
+        }
+
+        string result = string.Join("", sentences);
+        return result;
+    }
+
+    static string ReverseSentence(string sentence)
+    {
+        string[] words = sentence.Split(' ');
+
+        Array.Reverse(words);
+
+        string reversedSentence = string.Join(" ", words);
+
+        return reversedSentence;
+    }
+}
---
+++ Task 3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string downloadsPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+        string answersPath = downloadsPath + "\\Answers";
+
+        if (!Directory.Exists(answersPath))
+        {
+            Directory.CreateDirectory(answersPath);
+            Console.WriteLine("Директория 'Answers' успешно создана.");
+        }
+        else
+        {
+            Console.WriteLine("Директория 'Answers' уже существует.");
+        }
+
+        string cw1Path = answersPath + "\\cw_1.json";
+        if (!File.Exists(cw1Path))
+        {
+            File.Create(cw1Path).Close();
+            Console.WriteLine("Файл 'cw_1.json' успешно создана.");
+        }
+        else
+        {
+            Console.WriteLine("Файл 'cw_1.json' уже существует.");
+        }
+
+        string cw2Path = answersPath + "\\cw_2.json";
+        if (!File.Exists(cw2Path))
+        {
+            File.Create(cw2Path).Close();
+            Console.WriteLine("Файл 'cw_2.json' успешно создана.");
+        }
+        else
+        {
+            Console.WriteLine("Файл 'cw_2.json' уже существует.");
+        }
+    }
+}
