aae6920e7f66cc7c1d1634a23063239bdef52693
Reshetkov <64368633+Z1core@users.noreply.github.com>
2024-05-24 13:40:57 +0300

    Add files via upload

---
+++ KR1(Task1).cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public struct Number
+{
+    public int Real;
+
+    public static Number Add(Number num1, Number num2)
+    {
+        return new Number { Real = num1.Real + num2.Real };
+    }
+
+    public static Number Subtract(Number num1, Number num2)
+    {
+        return new Number { Real = num1.Real - num2.Real };
+    }
+
+    public static Number Multiply(Number num1, Number num2)
+    {
+        return new Number { Real = num1.Real * num2.Real };
+    }
+
+    public static Number Divide(Number num1, Number num2)
+    {
+        return new Number { Real = num1.Real / num2.Real };
+    }
+
+    public static void Display(Number num)
+    {
+        Console.WriteLine($"Number = {num.Real}");
+    }
+
+    public Number(int real)
+    {
+        Real = real;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Number num1 = new Number(10);
+        Number num2 = new Number(5);
+
+        Number sum = Number.Add(num1, num2);
+        Number difference = Number.Subtract(num1, num2);
+        Number product = Number.Multiply(num1, num2);
+        Number quotient = Number.Divide(num1, num2);
+
+        Number.Display(num1);
+        Number.Display(num2);
+        Number.Display(sum);
+        Number.Display(difference);
+        Number.Display(product);
+        Number.Display(quotient);
+    }
+}
---
+++ KR1(Task2).cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+public class Number
+{
+    protected int RealPart;
+
+    public Number(int realPart)
+    {
+        RealPart = realPart;
+    }
+
+    public static Number Add(Number num1, Number num2)
+    {
+        return new Number(num1.RealPart + num2.RealPart);
+    }
+
+    public static Number Subtract(Number num1, Number num2)
+    {
+        return new Number(num1.RealPart - num2.RealPart);
+    }
+
+    public static Number Multiply(Number num1, Number num2)
+    {
+        return new Number(num1.RealPart * num2.RealPart);
+    }
+
+    public static Number Divide(Number num1, Number num2)
+    {
+        if (num2.RealPart == 0)
+        {
+            Console.WriteLine("Деление на ноль недопустимо.");
+            return num1;
+        }
+
+        return new Number(num1.RealPart / num2.RealPart);
+    }
+
+    public virtual void Print()
+    {
+        Console.WriteLine($"Number = {RealPart}");
+    }
+}
+
+public class ComplexNumber : Number
+{
+    private int ImaginaryPart;
+
+    public ComplexNumber(int realPart, int imaginaryPart) : base(realPart)
+    {
+        ImaginaryPart = imaginaryPart;
+    }
+
+    public static ComplexNumber Add(ComplexNumber num1, ComplexNumber num2)
+    {
+        return new ComplexNumber(num1.RealPart + num2.RealPart, num1.ImaginaryPart + num2.ImaginaryPart);
+    }
+
+    public static ComplexNumber Subtract(ComplexNumber num1, ComplexNumber num2)
+    {
+        return new ComplexNumber(num1.RealPart - num2.RealPart, num1.ImaginaryPart - num2.ImaginaryPart);
+    }
+
+    public static ComplexNumber Multiply(ComplexNumber num1, ComplexNumber num2)
+    {
+        int realPart = num1.RealPart * num2.RealPart - num1.ImaginaryPart * num2.ImaginaryPart;
+        int imaginaryPart = num1.RealPart * num2.ImaginaryPart + num1.ImaginaryPart * num2.RealPart;
+        return new ComplexNumber(realPart, imaginaryPart);
+    }
+
+    public static ComplexNumber Divide(ComplexNumber num1, ComplexNumber num2)
+    {
+        if (num2.RealPart == 0 && num2.ImaginaryPart == 0)
+        {
+            Console.WriteLine("Деление на ноль недопустимо.");
+            return num1;
+        }
+
+        int realPart = (num1.RealPart * num2.RealPart + num1.ImaginaryPart * num2.ImaginaryPart) /
+                       (num2.RealPart * num2.RealPart + num2.ImaginaryPart * num2.ImaginaryPart);
+        int imaginaryPart = (num1.ImaginaryPart * num2.RealPart - num1.RealPart * num2.ImaginaryPart) /
+                            (num2.RealPart * num2.RealPart + num2.ImaginaryPart * num2.ImaginaryPart);
+        return new ComplexNumber(realPart, imaginaryPart);
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"Number = {RealPart} + {ImaginaryPart}i");
+    }
+}
+
+public class CWTask2
+{
+    public static void Main()
+    {
+        ComplexNumber num1 = new ComplexNumber(5, 4);
+        ComplexNumber num2 = new ComplexNumber(3, -2);
+
+        ComplexNumber sum = ComplexNumber.Add(num1, num2);
+        ComplexNumber difference = ComplexNumber.Subtract(num1, num2);
+        ComplexNumber product = ComplexNumber.Multiply(num1, num2);
+        ComplexNumber quotient = ComplexNumber.Divide(num1, num2);
+
+        num1.Print();
+        num2.Print();
+        sum.Print();
+        difference.Print();
+        product.Print();
+        quotient.Print();
+    }
+}
